ba32ac8715c5e4174169cc617707d4f418366f03
Daniel5452 <132559979+Daniel5452@users.noreply.github.com>
2023-10-16 14:37:51 +0200

    Initial commit

---
+++ app/src/androidTest/java/uicode.java
@@ -0,0 +1,15 @@
+//import android.os.Bundle;
+//import android.widget.Button;
+//import android.widget.SeekBar;
+//import android.widget.Switch;
+//import android.widget.Toast;
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//public class VestSettingsActivity extends AppCompatActivity {
+//
+//    private Switch compressionToggle;
+//    private SeekBar compressionLevelSeekBar;
+//    private Switch vibrationNotificationToggle;
+//    private Switch vibrationToggle;
+//    private Button automaticCompressionButton;
+//    private Button manualCompressionButton;
